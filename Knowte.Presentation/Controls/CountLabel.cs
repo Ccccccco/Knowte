eea47dec8ef7fb8bb57204dc28dbf2757d74fd11
Raphaël <raphael@digimezzo.com>
2018-02-08 17:05:44 +0100

    Added notes pivot header items, tweaked settings and information screen layout.

---
+++ Knowte.Presentation/Controls/CountLabel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Knowte.Presentation.Controls
+{
+    public class CountLabel : Label
+    {
+        static CountLabel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CountLabel), new FrameworkPropertyMetadata(typeof(CountLabel)));
+        }
+    }
+}
---
+++ Knowte.Presentation/Controls/NotesPivotItemHeader.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Knowte.Presentation.Controls
+{
+    public class NotesPivotItemHeader : Control
+    {
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register(nameof(IsSelected), typeof(bool), typeof(NotesPivotItemHeader), new PropertyMetadata(false));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(NotesPivotItemHeader), new PropertyMetadata(null));
+
+        public int Count
+        {
+            get { return (int)GetValue(CountProperty); }
+            set { SetValue(CountProperty, value); }
+        }
+
+        public static readonly DependencyProperty CountProperty =
+            DependencyProperty.Register(nameof(Count), typeof(int), typeof(NotesPivotItemHeader), new PropertyMetadata(0));
+
+        static NotesPivotItemHeader()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NotesPivotItemHeader), new FrameworkPropertyMetadata(typeof(NotesPivotItemHeader)));
+        }
+    }
+}
