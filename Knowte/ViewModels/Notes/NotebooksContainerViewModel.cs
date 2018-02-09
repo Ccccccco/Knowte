24a6b813caf80c51407ce1f3aafab4011264d000
Raphaël <raphael.godart@outlook.com>
2018-02-09 15:56:50 +0100

    Building the notebooks container

---
+++ Knowte/ViewModels/Notes/NotebooksContainerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Mvvm;
+
+namespace Knowte.ViewModels.Notes
+{
+    public class NotebooksContainerViewModel : BindableBase
+    {
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+            set { SetProperty<int>(ref this.count, value); }
+        }
+
+    }
+}
