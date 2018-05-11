389cadc40147a3d764357a31a0b3593bbda8be13
Raphaël <raphael.godart@outlook.com>
2018-05-11 13:14:26 +0200

    Added note icon

---
+++ Knowte/Controls/NoteIcon.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Knowte.Controls
+{
+    public partial class NoteIcon : UserControl
+    {
+        public Brush IconForeground
+        {
+            get { return (Brush)GetValue(IconForegroundProperty); }
+            set { SetValue(IconForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconForegroundProperty =
+            DependencyProperty.Register("IconForeground", typeof(Brush), typeof(NoteIcon), new PropertyMetadata(null));
+
+        public Brush IconBackground
+        {
+            get { return (Brush)GetValue(IconBackgroundProperty); }
+            set { SetValue(IconBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconBackgroundProperty =
+            DependencyProperty.Register("IconBackground", typeof(Brush), typeof(NoteIcon), new PropertyMetadata(null));
+
+        public NoteIcon()
+        {
+            InitializeComponent();
+        }
+    }
+}
