bd3a6fdddc49a1cc3b69aba6f7b7c96da79c1331
Raphaël <raphael.godart@outlook.com>
2018-05-20 10:47:16 +0200

    Implemented search in the service

--- Knowte.Services/Search/SearchService.cs
+++ Knowte.Services/Search/SearchService.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Timers;
 
 namespace Knowte.Services.Search
 {
     public class SearchService : ISearchService
     {
-        public string SearchText { get; set; }
+        private Timer searchTimer = new Timer();
+        private double searchTimeout = 0.2;
+
+        private string searchText;
+
+        public string SearchText
+        {
+            get { return this.searchText; }
+            set
+            {
+                this.searchText = value;
+                this.searchTimer.Start();
+            }
+        }
+
+        public SearchService()
+        {
+            this.searchTimer.Interval = TimeSpan.FromSeconds(this.searchTimeout).TotalMilliseconds;
+
+            this.searchTimer.Elapsed += (_, __) =>
+            {
+                this.searchTimer.Stop();
+                this.SearchTextChanged(this, new EventArgs());
+            };
+        }
 
         public event EventHandler SearchTextChanged = delegate { };
     }
